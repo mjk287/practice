cdfb303b871311b9a2eeebd6f1b7f69d96a5519b
mjk287 <myung.kim287@gmail.com>
2019-06-07 17:31:03 -0400

    able to find the most frequent char in a str with mapping a char object, and iterating over this object with Object entries instead of 'for in'

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let stringHash = {}
+  let mostChar
+  let prevNum = 0
+
+  for (let char of str) {
+    !stringHash[char] ? stringHash[char] = 1 : stringHash[char]++
+  }
+
+  Object.entries(stringHash).forEach((entry) => {
+    if (entry[1] > prevNum){
+      prevBiggestChar = entry[0]
+      prevNum = entry[1]
+    }
+  })
+
+  return prevBiggestChar
+}
+
+maxChar('Hello There!')
 
 module.exports = maxChar;
