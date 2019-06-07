a76e28da54ca0016a367579b28aa16a1f56ea033
mjk287 <myung.kim287@gmail.com>
2019-06-07 19:19:43 -0400

    solved anagrams through creating mapping hash and comparing characters

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,34 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // start by removing extraneous characters and capitalizations
+
+  const strA = stringA.replace(/[^\w]/g, '').toLowerCase().split('')
+  const strB = stringB.replace(/[^\w]/g, '').toLowerCase().split('')
+
+  if (strA.length !== strB.length){
+    return false
+  }
+
+  const strAHash = {}
+  const strBHash = {}
+
+  for (let char of strA) {
+    !strAHash[char] ? strAHash[char] = 1 : strAHash[char]++
+  }
+
+  for (let char of strB) {
+    !strBHash[char] ? strBHash[char] = 1 : strBHash[char]++
+  }
+
+  for (let char in strAHash) {
+    if (strAHash[char] !== strBHash[char]){
+      return false
+    }
+  }
+
+  return true
+}
 
 module.exports = anagrams;
