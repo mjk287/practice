995201ef92e4765944d8f2536861fe74d28ae010
mjk287 <myung.kim287@gmail.com>
2019-06-07 18:25:54 -0400

    solved chunked arrays first by a for loop iteration with conditionals. then solved it by incorporating slice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,40 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let lastIndex = 0
+  const chunked = []
+
+  while(lastIndex < array.length){
+    chunked.push(array.slice(lastIndex, lastIndex + size))
+    lastIndex += size
+  }
+
+  return chunked
+}
+
+// function chunk(array, size) {
+//   // iterate through array and append item to sub array based on a conditional
+//   // the conditional is that its length should be less than size. if so, append
+//   // sub array to origin array and empty sub array for next set of items
+//   // at the end append sub array if it has any items regardless
+//
+//   let subArr = []
+//   const newArr = []
+//
+//   for (let item of array){
+//     if (subArr.length === size){
+//       newArr.push(subArr)
+//       subArr = []
+//     }
+//     subArr.push(item)
+//   }
+//
+//   if (subArr.length !== 0){
+//     newArr.push(subArr)
+//   }
+//
+//   return newArr
+// }
 
 module.exports = chunk;
