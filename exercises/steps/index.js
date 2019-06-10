27e29cf78aad38fa50ac474d7ac0484f3a8891b2
mjk287 <myung.kim287@gmail.com>
2019-06-10 13:42:03 -0400

    solved steps problem with recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,43 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, rows = 0, stairs = '') {
+  // base case should compare current row with num of steps
+  if (rows === n ){
+    return;
+  }
+
+  // need to move on to next step
+  if (stairs.length === n){
+    console.log(stairs)
+    return steps(n, ++rows, stairs = '')
+  }
+
+  if (rows < stairs.length){
+    stairs += ' '
+  } else {
+    stairs += '#'
+  }
+
+  return steps(n, rows, stairs)
+}
+
+// function steps(n) {
+//   for (let i = 1; i <= n; i++){
+//     // number of punds to do
+//     let step = ''
+//     for (let x = 0; x < i; x++){
+//       step += '#'
+//     }
+//     for (let y = 0; y < (n - i); y++){
+//       step += ' '
+//     }
+//
+//   console.log(step)
+//   }
+//
+//
+// }
+
 
 module.exports = steps;
