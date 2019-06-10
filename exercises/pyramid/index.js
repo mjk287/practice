92f414e55bf66a97c8aa5a8ec5c94dba78d1e37e
mjk287 <myung.kim287@gmail.com>
2019-06-10 14:23:57 -0400

    solved the pyramid via finding the mid point and the appropriate range for the pound symbols

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,26 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // outer iteration for the rows
+  // inner iteration for the columns
+
+  for (let i = 0; i < n; i++){
+    let stairs = ''
+    const numOfCol = (n * 2 - 1)
+    // fill up a step with pound or empty
+    for (let x = 0; x < numOfCol; x++){
+      // if the current position is within this range, add a pound
+      const mid = n - 1
+      if (x <= mid + i && x >= mid - i){
+        stairs += '#'
+      }else{
+        stairs += ' '
+      }
+    }
+    console.log(stairs)
+  }
+}
+
 
 module.exports = pyramid;
