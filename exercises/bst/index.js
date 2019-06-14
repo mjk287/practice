9e798b1606af86fedb68b03e31456b26642ac998
mjk287 <myung.kim287@gmail.com>
2019-06-14 17:36:13 -0400

    implemented contains for a binary search tree that search for a node with a certain data

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,33 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data){
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+
+  insert(data){
+    // compare pass in data with its current data
+    // if less than then call insert on the left else right
+
+    if(data < this.data){
+      !!this.left ? this.left.insert(data) : this.left = new Node(data)
+    } else {
+      !!this.right ? this.right.insert(data) : this.right = new Node(data)
+    }
+  }
+
+  contains(data){
+    if(data < this.data){
+      return !!this.left ? this.left.contains(data) : null
+    } else if (data > this.data){
+      return !!this.right ? this.right.contains(data) : null
+    } else {
+      return this
+    }
+  }
+}
 
 module.exports = Node;
