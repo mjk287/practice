6d02fdb423ec42ed04d00ead39a6e4769da768db
mjk287 <myung.kim287@gmail.com>
2019-06-14 01:11:41 -0400

    now able to detech if linked list is circular or not

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,19 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+  let node1 = list.head
+  let node2 = list.head
+
+  while (!!node2.next && !!node2.next.next){
+      node1 = node1.next
+      node2 = node2.next.next
+      if (node1 === node2){
+        return true
+      }
+  }
+
+  return false
+}
 
 module.exports = circular;
