77d901e6d539205bf7c4ec9875b1b3f6a184a5dc
mjk287 <myung.kim287@gmail.com>
2019-06-14 16:33:52 -0400

    implemented width level for trees via keep track of 'level' and appending its length to counter arr

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let arr = [root]
+  let widths = [1]
+  let level = []
+
+  while(!!arr.length){
+    const currentNode = arr.shift()
+
+    level.push(...currentNode.children, )
+
+    if(!arr.length && !!level.length){
+      arr.push(...level)
+      widths.push(level.length)
+      level = []
+    }
+  }
+
+  return widths.filter(width => width !== 0)
+}
 
 module.exports = levelWidth;
