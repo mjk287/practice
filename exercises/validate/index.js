a5d92b1034572f585603cec18e7e26a835537f45
mjk287 <myung.kim287@gmail.com>
2019-06-14 20:21:53 -0400

    validates a binary search tree

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,38 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  // if (!!node){
+  //   if (node.data > max && node.data < min ){
+  //     return false
+  //   } else {
+  //     validate(node.left, min, node.data)
+  //     validate(node.right, node.data, max)
+  //   }
+  // }
+  // return true
+
+  //node must not be greater than max
+  if (!!max && !(node.data < max) ){
+    return false
+  }
+
+  if( !!min && !(node.data > min)){
+    return false
+  }
+
+  // check left side must be less than current node data
+  if(!!node.left && !validate(node.left, min, node.data)){
+    return false
+  }
+
+  // check right side must be greater than current node data
+  if(!!node.right && !validate(node.right, node.data, max)){
+    return false
+  }
+
+  return true
+
+}
 
 module.exports = validate;
