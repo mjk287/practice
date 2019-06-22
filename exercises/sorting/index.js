7ffdce56eaf9846085db1b66ec7d362009597d49
mjk287 <myung.kim287@gmail.com>
2019-06-21 22:23:14 -0400

    implemented bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,6 +3,17 @@
 
 function bubbleSort(arr) {
 
+  for (let i = 0; i < arr.length; i++) {
+    for (let x = 0; x < arr.length - i; x++) {
+      if (arr[x] > arr[x + 1]){
+        const temp = arr[x + 1]
+        arr[x + 1] = arr[x]
+        arr[x] = temp
+      }
+    }
+  }
+
+  return arr
 }
 
 function selectionSort(arr) {
